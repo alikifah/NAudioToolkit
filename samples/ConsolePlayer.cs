de3bb5894654dfb2dae753685339ac81a5c55902
Ali Kifah <olver422@yahoo.com>
2024-12-22 15:25:59 +0100

    Update ConsolePlayer.cs

--- samples/ConsolePlayer.cs
+++ samples/ConsolePlayer.cs
@@ -4,34 +4,35 @@ using System.Text;
 using System.Diagnostics;
 class Program
 {
-     static async Task Main(string[] args)
+     static void Main(string[] args)
      {
-            var p = new AudioPlayer<NAudio.Wave.WasapiOut>(@"https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav");
-            p.Play();
-            p.IsLoop = false;
-            p.OnPositionChanged += new Action<double>((d) => {
-
-                Console.WriteLine(d + "/" + p.Duration);
-            });
-
-            while (true)
+             using (var p = new AudioPlayer< NAudio.Wave.WasapiOut >(@"https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav"))
             {
-                var d = Console.ReadKey();
+                p.Play();
+                p.IsLoop = false;
+                p.OnPositionChanged += new Action<double>((d) =>
+                {
+                    Console.WriteLine(d + "/" + p.Duration);
+                });
 
-                if (d.Key == ConsoleKey.RightArrow)
-                    p.FastForward(0.5f);
-                else if (d.Key == ConsoleKey.LeftArrow)
-                    p.Rewind(0.5f);
+                while (true)
+                {
+                    var d = Console.ReadKey();
 
-                else if (d.Key == ConsoleKey.Spacebar && p.PlaybackState == NAudio.Wave.PlaybackState.Playing)
-                    p.Pause();
+                    if (d.Key == ConsoleKey.RightArrow)
+                        p.FastForward(0.5f);
+                    else if (d.Key == ConsoleKey.LeftArrow)
+                        p.Rewind(0.5f);
 
-                else if (d.Key == ConsoleKey.Spacebar && p.PlaybackState != NAudio.Wave.PlaybackState.Playing)
-                    p.Play();
+                    else if (d.Key == ConsoleKey.Spacebar && p.PlaybackState == NAudio.Wave.PlaybackState.Playing)
+                        p.Pause();
 
-                else if (d.Key == ConsoleKey.UpArrow)
-                    Console.WriteLine((int)(p.CurrentPosition / p.Duration * 100) + " %");
+                    else if (d.Key == ConsoleKey.Spacebar && p.PlaybackState != NAudio.Wave.PlaybackState.Playing)
+                        p.Play();
 
+                    else if (d.Key == ConsoleKey.UpArrow)
+                        Console.WriteLine((int)(p.CurrentPosition / p.Duration * 100) + " %");
+                }
             }
 
             Process.GetCurrentProcess().WaitForExit();
